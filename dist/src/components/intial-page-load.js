1af10eba115570906ac9972e2b41e54668372017
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-03 17:39:53 +0100

    Trying to fix last pull request

---
+++ dist/src/components/intial-page-load.js
@@ -0,0 +1,52 @@
+import '../styles/loader-styles.css'
+import '../global-styles.css'
+
+export default function initialPageLoad() {
+  const mainDiv = document.getElementById('main')
+  
+  const form = document.createElement('form')
+  form.id = 'form'
+  mainDiv.appendChild(form)
+  
+  const locationLabel = document.createElement('label')
+  locationLabel.setAttribute('for', 'location')
+  locationLabel.innerHTML = 'Location:'
+  form.appendChild(locationLabel)
+  
+  const locationInput = document.createElement('input')
+  locationInput.type = 'text'
+  locationInput.id = 'location'
+  locationInput.value = 'Berlin'
+  locationInput.setAttribute('name', 'location')
+  form.appendChild(locationInput)
+
+  const locationSubmitBtn = document.createElement('button')
+  locationSubmitBtn.type = 'submit'
+  locationSubmitBtn.innerHTML = 'Go'
+  form.appendChild(locationSubmitBtn)
+
+  const locationDiv = document.createElement('div')
+  mainDiv.appendChild(locationDiv)
+
+  const city = document.createElement('h2')
+  city.id = 'city'
+  locationDiv.appendChild(city)
+  
+  const country = document.createElement('h2')
+  country.id = 'country'
+  locationDiv.appendChild(country)
+
+  const currentWeatherDiv = document.createElement('div')
+  mainDiv.appendChild(currentWeatherDiv)
+
+  const currentWeather = document.createElement('h3')
+  currentWeather.classList.add('currentWeather')
+  currentWeather.id = 'currentWeather'
+  currentWeatherDiv.appendChild(currentWeather)
+
+  const currentTemp = document.createElement('h3')
+  currentTemp.classList.add('currentTemp')
+  currentTemp.id = 'currentTemp'
+  currentWeatherDiv.appendChild(currentTemp)
+}
+
